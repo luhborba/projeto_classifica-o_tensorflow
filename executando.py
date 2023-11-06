3880092e64b4c8c9b89d605800a24f87416a4710
Luciano Borba <luhborbafilho@gmail.com>
2023-11-06 13:18:23 -0300

    Ajustando detalhes

--- executando.py
+++ executando.py
@@ -3,12 +3,12 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Carregando Modelo
-model = load_model('modelo_de_animal_2.h5')
+model = load_model('modelo_de_animal.h5')
 
 img_width, img_height = 150, 150 # Tamanho das imagens
 batch_size = 32 #Tamanho do lote
 
-# Testando modelo
+# Processando Modelo
 def preprocess_image(image_path):
     img = image.load_img(image_path, target_size=(img_width, img_height))
     img = image.img_to_array(img)
@@ -17,21 +17,21 @@ def preprocess_image(image_path):
     return img
 
 # Carregando e imagem
-img_path = 'passaro_teste.jpg'  # Substitua pelo caminho da sua imagem
+img_path = 'cachorro_teste.jpg'  # Substitua pelo caminho da sua imagem
 img = preprocess_image(img_path)
 
-# Defina as etiquetas das classes
+# Definindo as etiquetas das classes
 class_labels = ['cachorro', 'cavalo', 'gato', 'passaro']
 
 # Previsão
 predictions = model.predict(img)
 
-# Obtendo prevista e probabilidade
+# Obtendo previção e probabilidade
 predicted_class = np.argmax(predictions)
 probability = predictions[0][predicted_class]
 
-# Mapeie a classe prevista de volta para o rótulo real (por exemplo, gato, cachorro, pássaro, peixe)
+# Mapeando a classe prevista de volta para o rótulo real
 predicted_label = class_labels[predicted_class]
 
-print("Animal identificado:", predicted_label)
-print("Probabilidade:", probability)
+print("Animal identificado: ", predicted_label)
+print("Probabilidade de: ", probability)
